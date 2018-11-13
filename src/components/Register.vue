1711d85d70f59ae6a07a03fd81c7e6407dae2486
waltermitch <devmitch256@gmail.com>
2018-11-13 18:10:50 +0100

    Added Vuetify to add design to the project.

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,13 +1,28 @@
 <template>
-    <div>
-        <h1>REGISTER</h1>
-
-        <form method="post" >
-            <input type="email" v-model="email" name="email" id="email" placeholder="Email Address"> <br>
-            <input type="password" v-model="password" name="passowrd" id="password" placeholder="Enter Password"> <br>
-            <button @click="handleRegister()" type="submit">Create User</button>
-        </form>
-    </div>
+    <v-layout row>
+        <v-flex xs6 offset-xs3>
+            <div class="white elevation-2">
+                <v-toolbar flat dense class="cyan" dark>
+                <v-toolbar-title>Register</v-toolbar-title>
+                </v-toolbar>
+                <v-alert :value="true" type="info">
+                    This is a error alert.
+                </v-alert>
+                <div class="pl-4 pr-4 pt-2 pb-2">
+                    <!--
+                    <v-input type="email" v-model="email" name="email" id="email" placeholder="Email Address"> </v-input><br>
+                    <v-input type="password" v-model="password" name="passowrd" id="password" placeholder="Enter Password"> </v-input><br>
+                    -->
+                    <v-text-field label="Email Address" v-model="email"></v-text-field>
+                    <v-text-field label="Password" v-model="password" type="password"></v-text-field>
+                    <!--<div class="error" v-html="error"></div>-->
+                    
+                    <v-btn class="cyan" @click="handleRegister()" type="submit">Create User</v-btn>
+                </div>
+            </div>
+        </v-flex>
+    </v-layout>
+    
 </template>
 <script>
 import AuthenticationService from '@/services/AuthenticationService'
@@ -15,21 +30,23 @@ export default {
     data () {
         return {
             email: '',
-            password: ''
+            password: '',
+            error: null
         }
     },
     methods : {
         async handleRegister(){
-            //e.preventDefault()
-            const response =  await AuthenticationService.register({
-                email: this.email,
-                password: this.password
-            })
-            console.log(response.data)
+            try {
+                await AuthenticationService.register({
+                    email: this.email,
+                    password: this.password
+                })
+            } catch (error) {
+                this.error = error.response.data.error
+            }
         }
     }
 }
 </script>
 <style scoped>
-
 </style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuetify from 'vuetify'
+//import 'vuetify/dist/vuetify.min.css'
+import('../node_modules/vuetify/dist/vuetify.min.css')
 
 Vue.config.productionTip = false
+
+Vue.use(Vuetify)
 
 new Vue({
   router,
